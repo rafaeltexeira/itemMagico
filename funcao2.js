00d7d8e1bb2b1cb1041c8c5247b6b86794df134b
rafaeltexeira <rafaeltexeira@outlook.com>
2024-07-30 18:51:28 -0300

    calculadora de rank

---
+++ funcao2.js
@@ -0,0 +1,26 @@
+function calcularRank(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = 'Ferro';
+    } else if (vitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (vitorias <= 50) {
+        nivel = 'Prata';
+    } else if (vitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (vitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (vitorias <= 100) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso
+const resultado = calcularRank(85, 20);
+console.log(resultado); // "O Herói tem de saldo de 65 está no nível de Diamante"
